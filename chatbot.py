1226653cb21b50eaa21ac27a25dbe521cdf5410f
supersergiy <sergiy.popovich@gmail.com>
2017-01-31 13:36:56 -0500

    cleanup

--- chatbot.py
+++ chatbot.py
@@ -21,8 +21,6 @@ def main():
                        help='number of characters to sample')
     parser.add_argument('--prime', type=str, default=' ',
                        help='prime text')
-    parser.add_argument('--beam_width', type=int, default=1,
-                       help='Width of the beam for beam search, default 2')
     parser.add_argument('--temperature', type=float, default=1.,
                        help='sampling temperature'
                        '(lower is more conservative, default is 1.0, which is neutral)')
@@ -75,9 +73,7 @@ def sample_main(args):
         # Restore the saved variables, replacing the initialized values.
         print("Restoring weights...")
         saver.restore(sess, model_path)
-        chatbot(net, sess, chars, vocab, args.n, args.beam_width, args.relevance, args.temperature)
-        #beam_sample(net, sess, chars, vocab, args.n, args.prime,
-            #args.beam_width, args.relevance, args.temperature)
+        chatbot(net, sess, chars, vocab, args.n, args.relevance, args.temperature)
 
 def initial_state(net, sess):
     # Return freshly initialized model states.
@@ -107,19 +103,6 @@ def scale_prediction(prediction, temperature):
     np.seterr(divide='warn')
     return scaled_prediction
 
-def beam_sample(net, sess, chars, vocab, max_length=200, prime='The ',
-    beam_width = 2, relevance=3.0, temperature=1.0):
-    states = [initial_state(net, sess), initial_state(net, sess)]
-    states = forward_text(net, sess, states, vocab, prime)
-    computer_response_generator = beam_search_generator(sess, net, states, vocab[' '],
-        None, beam_width, forward_with_mask, (temperature, vocab['\n']))
-    for i, char_token in enumerate(computer_response_generator):
-        print(chars[char_token], end='')
-        states = forward_text(net, sess, states, vocab, chars[char_token])
-        sys.stdout.flush()
-        if i >= max_length: break
-    print()
-
 def sanitize_text(vocab, text):
     return ''.join(i for i in text if i in vocab)
 
@@ -127,18 +110,18 @@ def initial_state_with_relevance_masking(net, sess, relevance):
     if relevance <= 0.: return initial_state(net, sess)
     else: return [initial_state(net, sess), initial_state(net, sess)]
 
-def chatbot(net, sess, chars, vocab, max_length, beam_width, relevance, temperature):
+def chatbot(net, sess, chars, vocab, max_length, relevance, temperature):
     states = initial_state_with_relevance_masking(net, sess, relevance)
     while True:
         user_input = sanitize_text(vocab, raw_input('\n> '))
-        user_command_entered, reset, states, relevance, temperature, beam_width = process_user_command(
-            user_input, states, relevance, temperature, beam_width)
+        user_command_entered, reset, states, relevance, temperature = process_user_command(
+            user_input, states, relevance, temperature)
         if reset: states = initial_state_with_relevance_masking(net, sess, relevance)
         if user_command_entered: continue
         states = forward_text(net, sess, states, vocab, '> ' + user_input + "\n>")
         computer_response_generator = beam_search_generator(sess=sess, net=net,
             initial_state=copy.deepcopy(states), initial_sample=vocab[' '],
-            early_term_token=vocab['\n'], beam_width=beam_width, forward_model_fn=forward_with_mask,
+            early_term_token=vocab['\n'], forward_model_fn=forward_with_mask,
             forward_args=(relevance, vocab['\n']), temperature=temperature)
         for i, char_token in enumerate(computer_response_generator):
             print(chars[char_token], end='')
@@ -147,7 +130,7 @@ def chatbot(net, sess, chars, vocab, max_length, beam_width, relevance, temperat
             if i >= max_length: break
         states = forward_text(net, sess, states, vocab, '\n> ')
 
-def process_user_command(user_input, states, relevance, temperature, beam_width):
+def process_user_command(user_input, states, relevance, temperature):
     user_command_entered = False
     reset = False
     try:
@@ -164,17 +147,13 @@ def process_user_command(user_input, states, relevance, temperature, beam_width)
                 states = states[0]
             relevance = new_relevance
             print("[Relevance disabled]" if relevance < 0. else "[Relevance set to {}]".format(relevance))
-        elif user_input.startswith('--beam_width '):
-            user_command_entered = True
-            beam_width = max(1, int(user_input[len('--beam_width '):]))
-            print("[Beam width set to {}]".format(beam_width))
         elif user_input.startswith('--reset'):
             user_command_entered = True
             reset = True
             print("[Model state reset]")
     except ValueError:
         print("[Value error with provided argument.]")
-    return user_command_entered, reset, states, relevance, temperature, beam_width
+    return user_command_entered, reset, states, relevance, temperature
 
 def consensus_length(beam_outputs, early_term_token):
     for l in xrange(len(beam_outputs[0])):
@@ -204,7 +183,7 @@ def forward_with_mask(sess, net, states, input_sample, forward_args):
     return combined_prob / sum(combined_prob), states
 
 def beam_search_generator(sess, net, initial_state, initial_sample,
-    early_term_token, beam_width, forward_model_fn, forward_args, temperature):
+    early_term_token, forward_model_fn, forward_args, temperature):
     '''Run beam search! Yield consensus tokens sequentially, as a generator;
     return when reaching early_term_token (newline).
 
@@ -216,7 +195,6 @@ def beam_search_generator(sess, net, initial_state, initial_sample,
             to start the generation
         early_term_token: stop when the beam reaches consensus on this token
             (but do not return this token).
-        beam_width: how many beams to track
         forward_model_fn: function to forward the model, must be of the form:
             probability_output, beam_state =
                     forward_model_fn(sess, net, beam_state, beam_sample, forward_args)
@@ -226,79 +204,27 @@ def beam_search_generator(sess, net, initial_state, initial_sample,
         tot_steps: how many tokens to generate before stopping,
             unless already stopped via early_term_token.
     Returns: a generator to yield a sequence of beam-sampled tokens.'''
-    # Store state, outputs and probabilities for up to args.beam_width beams.
-    # Initialize with just the one starting entry; it will branch to fill the beam
-    # in the first step.
-    beam_states = [initial_state] # Stores the best activation states
-    beam_outputs = [[initial_sample]] # Stores the best generated output sequences so far.
-    beam_probs = [1.] # Stores the cumulative normalized probabilities of the beams so far.
+    state = initial_state # Stores the best activation states
+    sample = initial_sample
 
     while True:
         # Keep a running list of the best beam branches for next step.
         # Don't actually copy any big data structures yet, just keep references
         # to existing beam state entries, and then clone them as necessary
         # at the end of the generation step.
-        new_beam_indices = []
-        new_beam_probs = []
-        new_beam_samples = []
 
-        # Iterate through the beam entries.
-        for beam_index, beam_state in enumerate(beam_states):
-            beam_prob = beam_probs[beam_index]
-            beam_sample = beam_outputs[beam_index][-1]
+        prediction, state = forward_model_fn(
+                    sess, net, state, sample, forward_args)
+        prediction = scale_prediction(prediction, temperature)
 
-            # Forward the model.
-            prediction, beam_states[beam_index] = forward_model_fn(
-                    sess, net, beam_state, beam_sample, forward_args)
-            prediction = scale_prediction(prediction, temperature)
+        token = np.random.choice(len(prediction), p=prediction)
 
-            # Sample best_tokens from the probability distribution.
-            # Sample from the scaled probability distribution beam_width choices
-            # (but not more than the number of positive probabilities in scaled_prediction).
-            count = min(beam_width, sum(1 if p > 0. else 0 for p in prediction))
-            best_tokens = np.random.choice(len(prediction), size=count,
-                                            replace=False, p=prediction)
-            for token in best_tokens:
-                prob = prediction[token] * beam_prob
-                if len(new_beam_indices) < beam_width:
-                    # If we don't have enough new_beam_indices, we automatically qualify.
-                    new_beam_indices.append(beam_index)
-                    new_beam_probs.append(prob)
-                    new_beam_samples.append(token)
-                else:
-                    # Sample a low-probability beam to possibly replace.
-                    np_new_beam_probs = np.array(new_beam_probs)
-                    inverse_probs = -np_new_beam_probs + max(np_new_beam_probs) + min(np_new_beam_probs)
-                    inverse_probs = inverse_probs / sum(inverse_probs)
-                    sampled_beam_index = np.random.choice(beam_width, p=inverse_probs)
-                    if new_beam_probs[sampled_beam_index] <= prob:
-                        # Replace it.
-                        new_beam_indices[sampled_beam_index] = beam_index
-                        new_beam_probs[sampled_beam_index] = prob
-                        new_beam_samples[sampled_beam_index] = token
-        # Replace the old states with the new states, first by referencing and then by copying.
-        already_referenced = [False] * beam_width
-        new_beam_states = []
-        new_beam_outputs = []
-        for i, new_index in enumerate(new_beam_indices):
-            if already_referenced[new_index]:
-                new_beam = copy.deepcopy(beam_states[new_index])
-            else:
-                new_beam = beam_states[new_index]
-                already_referenced[new_index] = True
-            new_beam_states.append(new_beam)
-            new_beam_outputs.append(beam_outputs[new_index] + [new_beam_samples[i]])
-        # Normalize the beam probabilities so they don't drop to zero
-        beam_probs = new_beam_probs / sum(new_beam_probs)
-        beam_states = new_beam_states
-        beam_outputs = new_beam_outputs
-        # Prune the agreed portions of the outputs
-        # and yield the tokens on which the beam has reached consensus.
-        l, early_term = consensus_length(beam_outputs, early_term_token)
-        if l > 0:
-            for token in beam_outputs[0][:l]: yield token
-            beam_outputs = [output[l:] for output in beam_outputs]
-        if early_term: return
+        if (token == early_term_token):
+            return
+        else:
+            yield token
+
+        sample = token
 
 if __name__ == '__main__':
     main()
